4914fde03e61e587e2d5decd4a09116b69c80adc
Faizan Awan <mohammadfaizan980@gmail.com>
2019-05-27 11:01:38 +0500

    Added empty activity for topics

---
+++ app/src/main/java/com/squadtechs/faizan/sla/activity_topics/ActivityTopics.java
@@ -0,0 +1,14 @@
+package com.squadtechs.faizan.sla.activity_topics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.squadtechs.faizan.sla.R;
+
+public class ActivityTopics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_topics);
+    }
+}
