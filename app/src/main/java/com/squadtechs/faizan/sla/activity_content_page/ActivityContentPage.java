d4ff377808749e4a2075c301a4d4772c7a7d341d
Faizan Awan <mohammadfaizan980@gmail.com>
2019-05-28 10:57:24 +0500

    Added XML for content activity

--- app/src/main/java/com/squadtechs/faizan/sla/activity_content_page/ActivityContentPage.java
+++ app/src/main/java/com/squadtechs/faizan/sla/activity_content_page/ActivityContentPage.java
@@ -2,13 +2,22 @@ package com.squadtechs.faizan.sla.activity_content_page;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.github.barteksc.pdfviewer.PDFView;
 import com.squadtechs.faizan.sla.R;
 
 public class ActivityContentPage extends AppCompatActivity {
+
+    private PDFView pdfView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content_page);
+        initViews();
     }
+
+    private void initViews() {
+        pdfView = findViewById(R.id.pdfView);
+    }
+
 }
