1b6ee6583f8c9d1a2ce987e257b85adb846258a2
Faizan Awan <mohammadfaizan980@gmail.com>
2019-05-28 11:43:46 +0500

    CHanged UI a bit

--- app/src/main/java/com/squadtechs/faizan/sla/activity_content_page/ActivityContentPage.java
+++ app/src/main/java/com/squadtechs/faizan/sla/activity_content_page/ActivityContentPage.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.WindowManager;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -22,6 +23,10 @@ public class ActivityContentPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content_page);
         initViews();
@@ -46,7 +51,24 @@ public class ActivityContentPage extends AppCompatActivity {
     }
 
     private void loadData() {
+        String mURL = "https://firebasestorage.googleapis.com/v0/b/student-learning-app.appspot.com/o/HIVinfobooklet.pdf?alt=media&token=536cf84b-1d2d-4f6e-b83f-c494fd836ae2";
+        RequestQueue requestQueue = Volley.newRequestQueue(ActivityContentPage.this);
+        InputStreamVolleyRequest request = new InputStreamVolleyRequest(Request.Method.GET, mURL, new Response.Listener<byte[]>() {
+            @Override
+            public void onResponse(byte[] response) {
+                InputStream stream = new ByteArrayInputStream(response);
+                pdfView.fromStream(stream)
+                        .enableSwipe(true)
+                        .defaultPage(0)
+                        .load();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
 
+            }
+        }, null);
+        requestQueue.add(request);
     }
 
 }
