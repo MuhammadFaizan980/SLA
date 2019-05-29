a04eaf75f273b69e63713ed5f324c160861460c5
Faizan Awan <mohammadfaizan980@gmail.com>
2019-05-29 11:27:31 +0500

    Added XML for play video activity

--- app/src/main/java/com/squadtechs/faizan/sla/activity_play_video/ActivityPlayVideo.java
+++ app/src/main/java/com/squadtechs/faizan/sla/activity_play_video/ActivityPlayVideo.java
@@ -3,9 +3,12 @@ package com.squadtechs.faizan.sla.activity_play_video;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.VideoView;
 import com.squadtechs.faizan.sla.R;
 
 public class ActivityPlayVideo extends AppCompatActivity {
+
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +18,11 @@ public class ActivityPlayVideo extends AppCompatActivity {
         );
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_video);
+        initViews();
     }
+
+    private void initViews() {
+        videoView = findViewById(R.id.video_play);
+    }
+
 }
