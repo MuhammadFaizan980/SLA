f3a335675d92bbbaedb5f9021029b00bdcdb8937
Faizan Awan <mohammadfaizan980@gmail.com>
2019-06-11 18:10:56 +0500

    Added empty question activity and question comments activity

---
+++ app/src/main/java/com/squadtechs/faizan/sla/activity_question_comments/ActivityQuestionComments.java
@@ -0,0 +1,15 @@
+package com.squadtechs.faizan.sla.activity_question_comments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.squadtechs.faizan.sla.R;
+
+public class ActivityQuestionComments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_comments);
+    }
+}
