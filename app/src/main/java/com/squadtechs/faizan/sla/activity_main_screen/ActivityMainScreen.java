72b6e95b1e26f67e17aa3de943b88c4607059cc3
Faizan Awan <mohammadfaizan980@gmail.com>
2019-06-12 21:03:45 +0500

    Added menu for question activity  in the main screen

--- app/src/main/java/com/squadtechs/faizan/sla/activity_main_screen/ActivityMainScreen.java
+++ app/src/main/java/com/squadtechs/faizan/sla/activity_main_screen/ActivityMainScreen.java
@@ -1,18 +1,22 @@
 package com.squadtechs.faizan.sla.activity_main_screen;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.Toast;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squadtechs.faizan.sla.R;
+import com.squadtechs.faizan.sla.activity_questions.ActivityQuestions;
 
 import java.util.ArrayList;
 
@@ -45,6 +49,19 @@ public class ActivityMainScreen extends AppCompatActivity {
 
     private void populateToolbar() {
         toolbar.setTitle("Languages");
+        toolbar.inflateMenu(R.menu.menu_question);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.item_questions: {
+                        startActivity(new Intent(ActivityMainScreen.this, ActivityQuestions.class));
+                        finish();
+                    }
+                }
+                return true;
+            }
+        });
     }
 
     private void prepareRecyclerView() {
